5a49f931b6c9a8680b0641beebe595c994442f28
Wayne Muir <waynemuir.ece@gmail.com>
2024-05-14 19:49:23 -0600

    Added test

--- i_ExpenseAveragerTests/Repositories/VeiwRepositoryModelTests.cs
+++ i_ExpenseAveragerTests/Repositories/VeiwRepositoryModelTests.cs
@@ -93,5 +93,41 @@ namespace i_ExpenseAveragerTests.Repositories
             Assert.AreEqual("$10.16", category.DailyAvg);
             Assert.AreEqual("$71.12", category.MonthAvg);
         }
+
+        [TestMethod]
+        public void RefreshDisplay_ShouldCalculateAveragesCorrectlyOverOneWeek()
+        {
+            // Arrange
+            _mockXDB.Setup(m => m.StartDate).Returns(DateTime.Now.AddDays(-7));
+            ExpenseAverages expenses = new ExpenseAverages();
+            expenses.Add(new ExpenseAverage(1, 1, 1, 1, DateTime.Now.AddDays(-1), 15.24, ""));
+            expenses.Add(new ExpenseAverage(1, 1, 1, 1, DateTime.Now.AddDays(-1), 15.24, ""));
+            expenses.Add(new ExpenseAverage(1, 1, 1, 1, DateTime.Now.AddDays(-1), 15.24, ""));
+            expenses.Add(new ExpenseAverage(1, 1, 1, 1, DateTime.Now.AddDays(-1), 15.24, ""));
+            _mockXDB.Setup(m => m.ExpenseAverages).Returns(expenses);
+
+            var viewRepositoryModel = new ViewRepositoryModel(_mockXDB.Object);
+            var category = new ExpenseAverageCategory("TestCategory");
+            ExpenseTag consumable = new ExpenseTag("Consumable");
+            consumable.ExpenseTagID = 1;
+            consumable.ExpenseTagType = "type";
+            category.Tags.Add(consumable);
+            ExpenseTag gasoline = new ExpenseTag("Gasoline");
+            gasoline.ExpenseTagID = 2;
+            gasoline.ExpenseTagType = "type";
+            category.Tags.Add(gasoline);
+            ExpenseTag medical = new ExpenseTag("Medical");
+            medical.ExpenseTagID = 3;
+            medical.ExpenseTagType = "type";
+            category.Tags.Add(medical);
+
+            // Act
+            var result = viewRepositoryModel.RefreshDisplay(category);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("$7.62", category.DailyAvg);
+            Assert.AreEqual("$53.34", category.MonthAvg);
+        }
     }
 }
