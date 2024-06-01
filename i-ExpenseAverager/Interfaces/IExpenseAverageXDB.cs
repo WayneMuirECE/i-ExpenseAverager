1d1e00843bf42e9213a25c92e906a85243e7cea7
Wayne Muir <waynemuir.ece@gmail.com>
2024-05-31 20:29:27 -0600

    Added comment blocks

--- i-ExpenseAverager/Interfaces/IExpenseAverageXDB.cs
+++ i-ExpenseAverager/Interfaces/IExpenseAverageXDB.cs
@@ -12,29 +12,114 @@ namespace i_ExpenseAverager.Interfaces
         ExpenseTags ExpenseOccasions { get; set; }
         ExpenseAverages ExpenseAverages { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseAverageTypeID"></param>
+        /// <returns></returns>
         ExpenseTag GetExpenseAverageType(int expenseAverageTypeID);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseAverageType"></param>
+        /// <returns></returns>
         ExpenseTag GetExpenseAverageType(string expenseAverageType);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseLocationID"></param>
+        /// <returns></returns>
         ExpenseTag GetExpenseLocation(int expenseLocationID);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseLocation"></param>
+        /// <returns></returns>
         ExpenseTag GetExpenseLocation(string expenseLocation);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseOccasionID"></param>
+        /// <returns></returns>
         ExpenseTag GetExpenseOccasion(int expenseOccasionID);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseOccasion"></param>
+        /// <returns></returns>
         ExpenseTag GetExpenseOccasion(string expenseOccasion);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseAverageTypeName"></param>
         void SaveExpenseAverageType(string expenseAverageTypeName);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseLocationName"></param>
         void SaveExpenseLocation(string expenseLocationName);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseOccasionName"></param>
         void SaveExpenseOccasion(string expenseOccasionName);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="location"></param>
+        /// <param name="occasion"></param>
+        /// <param name="forDate"></param>
+        /// <param name="amount"></param>
+        /// <param name="note"></param>
+        /// <returns></returns>
         bool SaveExpenseAverage(string type, string location, string occasion, DateTime forDate, double amount, string note);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expense"></param>
+        /// <returns></returns>
         bool DeleteExpenseAverage(ExpenseAverage expense);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseAverageTypeID"></param>
+        /// <returns></returns>
         List<ExpenseAverage> GetExpenseAverageForExpenseAverageType(int expenseAverageTypeID);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         ExpenseAverage GetCurrentExpenseAverageTypeLastExpenseAverage();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         bool CurrentExpenseAverageTypeSelected();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expenseAverageTypeName"></param>
         void SaveCurrentExpenseAverageType(string expenseAverageTypeName);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         ExpenseTag GetCurrentExpenseAverageType();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         List<ExpenseTag> GetExpenseAverageTypes();
 
+        /// <summary>
+        /// 
+        /// </summary>
         void SubmitChanges();
     }
 
